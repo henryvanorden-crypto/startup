6c6b86bb3c6fc45bf9fb46aaf44236f13507c98f
Henry Van Orden <henryvanorden@gmail.com>
2026-02-11 18:17:09 -0700

    creating jsx header, footer, and main placeholders

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    <header className="container-fluid">
+      <nav className="navbar fixed-top">
+        <a className="navbar-brand" href="index.html">TriviaDash</a>
+        <menu className="navbar-nav">
+          <li className="nav-item"><a href="index.html" className="nav-link active">Home</a></li>
+          <li className="nav-item"><a href="play.html" className="nav-link">Play</a></li>
+          <li className="nav-item"><a href="scores.html" className="nav-link">Scores</a></li>
+          <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
+        </menu>
+      </nav>
+    </header>
+
+    <main className="container-fluid back-light text-center">
+      This is main
+    </main>
+
+    <footer>
+      <div className="container-fluid">
+        <span className="text-reset">Henry Van Orden</span>
+        <a href="https://github.com/henryvanorden-crypto/startup">GitHub</a>
+      </div>
+    </footer>
+    </div>;
 }
