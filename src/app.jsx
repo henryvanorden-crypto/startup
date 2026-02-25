1c94c99947bf34cf4a526791a37a82b07afb8ebb
Henry Van Orden <henryvanorden@gmail.com>
2026-02-24 23:55:54 -0700

    login functionality

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes, Navigate} from 'react-router-dom';
 import { Login } from './login/login';
 import { Play } from './play/play';
 import { Scores } from './scores/scores';
 import { About } from './about/about';
 
 export default function App() {
+    const [user, setUser] = React.useState(localStorage.getItem('user') || null);
+function logout() {
+    localStorage.removeItem('user')
+    setUser(null)
+  }
+
   return (
     <BrowserRouter>
         <div className="body back-dark">
@@ -16,17 +22,23 @@ export default function App() {
             <nav className="navbar fixed-top">
                 <div className="navbar-brand">TriviaDash</div>
                 <menu className="navbar-nav">
-                <li className="nav-item"><NavLink to="/" className="nav-link">Home</NavLink></li>
-                <li className="nav-item"><NavLink to="play" className="nav-link">Play</NavLink></li>
-                <li className="nav-item"><NavLink to="scores" className="nav-link">Scores</NavLink></li>
+                <li className="nav-item"><NavLink to="/" className="nav-link">Login</NavLink></li>
+                {user && <li className="nav-item"><NavLink to="play" className="nav-link">Play</NavLink></li>}
+                {user && <li className="nav-item"><NavLink to="scores" className="nav-link">Scores</NavLink></li>}
                 <li className="nav-item"><NavLink to="about" className="nav-link">About</NavLink></li>
                 </menu>
+                {user}
+                {user && (
+              <button onClick={logout} className="btn sec-but">
+                Logout
+              </button>
+            )}
             </nav>
             </header>
             <main>
                 <Routes>
-                    <Route path='/' element={<Login />} exact />
-                    <Route path='/play' element={<Play />} />
+                    <Route path='/' element={!user ? <Login setUser={setUser} /> : <Navigate to="/play" />} exact />
+                    <Route path='/play' element={user ? <Play user={user} /> : <Navigate to="/" />} />
                     <Route path='/scores' element={<Scores />} />
                     <Route path='/about' element={<About />} />
                     <Route path='*' element={<NotFound />} />
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
 import '../app.css';
+import { useNavigate } from 'react-router-dom';
 
-export function Login() {
+export function Login({setUser}) {
+const [text, setText] = React.useState('')
+const [password, setPassword] = React.useState('');
+const [displayError, setDisplayError] = React.useState(null);
+const navigate = useNavigate()
+
+function loginUser(){
+  localStorage.setItem('user', text)
+  setUser(text)
+  navigate('/play')
+}
+function createUser(){
+  localStorage.setItem('user', text)
+  setUser(text)
+  navigate('/play')
+}
+
+function textChange(e){
+  setText(e.target.value)
+}
+
+function passChange(e){
+  setPassword(e.target.value)
+}
+
   return (
     <main className="container-fluid back-light text-center">
       <h1>Welcome to TriviaDash</h1>
-      <form method="get" action="play.html">
+      <form>
         <div className="input-group mb-3">
           <span className="input-group-text">@</span>
-          <input className="form-control" type="text" placeholder="your@email.com" />
+          <input onChange={textChange} className="form-control" type="text" placeholder="your@email.com" />
         </div>
         <div className="input-group mb-3">
           <span className="input-group-text">🔒</span>
-          <input className="form-control" type="password" placeholder="password" />
+          <input onChange={passChange} className="form-control" type="password" placeholder="password" />
         </div>
-        <button type="submit" className="btn but-color">Login</button>
-        <button type="submit" className="btn sec-but">Create</button>
+        <button onClick={loginUser} disabled={!text || !password} className="btn but-color">Login</button>
+        <button onClick={createUser} disabled={!text || !password} className="btn sec-but">Create</button>
       </form>
     </main>
+    
   );
 }
