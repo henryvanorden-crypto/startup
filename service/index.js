144768eeb7f104ad8f9700354e6a8bb27dc456f9
Henry Van Orden <henryvanorden@gmail.com>
2026-03-09 22:51:08 -0600

    setup index.js

--- service/index.js
+++ service/index.js
@@ -1,2 +1,22 @@
+const express = require('express');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
+
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+app.use(express.json());
+
+let users = [];
+let scores = [];
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
